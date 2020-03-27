8f330af89bd49a9a3bda4b3f8eb958287ee0e7b0
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-27 13:26:51 +0100

    calendar styled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
       <v-row>
         <v-col class="col-12">
           <v-card>
-            <v-date-picker v-model="date" full-width></v-date-picker>
+            <v-date-picker v-model="date" full-width locale="es-sp" :min="minimum" :max="maximum"></v-date-picker>
           </v-card>
           <v-card color="error" dark>
             <v-card-text class="display-1 text-center">
@@ -27,7 +27,9 @@ export default {
   name: "Home",
   data() {
     return {
-      date: ""
+      date: "",
+      minimum: "1999",
+      maximum: new Date().toISOString().substr(0,10)
     };
   }
 };
