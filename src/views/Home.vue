a9fdb773e4c31de0202594589bc4251b64569519
braisv <brais.vidal.rodriguez@gmail.com>
2020-03-30 12:25:01 +0200

    miindicador api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,11 +9,18 @@
       <v-row>
         <v-col class="col-12">
           <v-card>
-            <v-date-picker v-model="date" full-width locale="es-sp" :min="minimum" :max="maximum"></v-date-picker>
+            <v-date-picker
+              v-model="date"
+              full-width
+              locale="es-sp"
+              :min="minimum"
+              :max="maximum"
+              @change="getDolar(date)"
+            ></v-date-picker>
           </v-card>
           <v-card color="error" dark>
             <v-card-text class="display-1 text-center">
-              $: {{ date }}
+              {{ value }}€: {{ date }}
             </v-card-text>
           </v-card>
         </v-col>
@@ -23,14 +30,30 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Home",
   data() {
     return {
-      date: "",
+      date: new Date().toISOString().substr(0, 10),
       minimum: "1999",
-      maximum: new Date().toISOString().substr(0,10)
+      maximum: new Date().toISOString().substr(0, 10),
+      value: null
     };
+  },
+  methods: {
+    async getDolar(date) {
+      let sortedDay = date.split('-').reverse().join('-')
+      console.log('SORTED DAY: ', sortedDay)
+      console.log("AXIOS: ", axios);
+      let data = await axios.get(`https://mindicador.cl/api/euro/${sortedDay}`);
+      console.log("DATA: ", data.data.serie[0].valor);
+      this.value = await data.data.serie[0].valor;
+    }
+  },
+  created() {
+    this.getDolar(this.date);
   }
 };
 </script>
